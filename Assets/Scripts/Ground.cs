bec17d301cb28793e7602c82ee747433191b0eb1
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2019-04-01 20:01:08 +0200

    repop ground

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public bool currentGround;
+
+    public Transform respawnTransform;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Player") && !currentGround)
+        {
+            currentGround = true;
+
+            other.collider.gameObject.GetComponentInParent<PlayerMovement>().ChangeGround(this);
+        }
+    }
+}
