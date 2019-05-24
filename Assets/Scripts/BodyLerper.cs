571ea6dddedcbb72cf8576a07a64fb17c17efb53
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2019-05-24 18:08:37 +0200

    stop mo deplacement + leg v1

---
+++ Assets/Scripts/BodyLerper.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyLerper : MonoBehaviour
+{
+    public float fps = 1;
+
+    public Transform target;
+
+    public bool lerping;
+
+    public bool lerpLeg;
+
+    public Leg[] legs;
+    public bool rightLeg;
+
+    public Rigidbody rb;
+    void Start()
+    {
+        StartCoroutine(UpdateBody());
+        StartCoroutine(UpdateLegs());
+    }
+
+    private void Update()
+    {
+        fps += Input.mouseScrollDelta.y;
+        Mathf.Clamp(fps, 1, 100);
+    }
+
+    IEnumerator UpdateBody()
+    {
+        while (true)
+        {
+            transform.SetPositionAndRotation(target.position, target.rotation);
+            if (lerping)
+            {
+                yield return new WaitForSeconds(1/fps);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+    IEnumerator UpdateLegs()
+    {
+        while (true)
+        {
+
+            if (rightLeg)
+            {
+                legs[0].UpdateLeg(rb.velocity);
+            }
+            else
+            {
+                legs[1].UpdateLeg(rb.velocity);
+            }
+
+            rightLeg = !rightLeg;
+            
+            
+            if (lerpLeg)
+            {
+                yield return new WaitForSeconds(1/fps);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Leg.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leg : MonoBehaviour
+{
+    public Transform foot;
+    public Transform hip;
+
+    private RaycastHit hit;
+
+    public bool lerping;
+
+    public float fps = 4;
+
+    public float forwardDistance = 1;
+
+    public float rayDistance = 2;
+    
+    private void Start()
+    {
+        foot.transform.parent = null;
+        //StartCoroutine(UpdateBody());
+    }
+
+    private void Update()
+    {
+        hip.eulerAngles = foot.eulerAngles;
+        //hip.LookAt(foot);
+        //hip.localEulerAngles += new Vector3(90,-90,180);
+    }
+
+
+    IEnumerator UpdateBody()
+    {
+        while (true)
+        {
+            //UpdateLeg();
+            
+            if (lerping)
+            {
+                yield return new WaitForSeconds(1/fps);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+    public void UpdateLeg(Vector3 _vel)
+    {
+        Vector3.ClampMagnitude(_vel, 1);
+        
+        _vel.y = 0;
+        
+        if (Physics.Raycast(transform.position, Vector3.down + _vel * forwardDistance, out hit, rayDistance))
+        {
+            foot.position = hit.point;
+            foot.transform.up = hit.normal;
+            foot.transform.eulerAngles = new Vector3(foot.transform.eulerAngles.x, transform.eulerAngles.y - 90f, foot.transform.eulerAngles.z);
+        }
+    }
+}
