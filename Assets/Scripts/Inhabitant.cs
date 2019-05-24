6b19a82a7618722e056019b7a0a3738ebedddb60
rackover <rackover@live.fr>
2019-05-24 17:36:29 +0200

    Laid out first elements for NPC system

---
+++ Assets/Scripts/Inhabitant.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Inhabitant : MonoBehaviour
+{
+    public enum TEAM { TOURIST, INHABITANT, STAFF, ANIMAL };
+    public TEAM team;
+    public InhabitantSheet sheet;
+
+    NavMeshAgent navMeshAgent;
+    GameObject visual;
+
+    private void Awake()
+    {
+        if (sheet == null) throw new MissingComponentException("Inhabitant has no sheet : " + name);
+
+        navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
+        navMeshAgent.areaMask = sheet.navMeshAreaMask;
+        navMeshAgent.speed = sheet.walkingSpeed;
+        navMeshAgent.angularSpeed = 720f;
+        navMeshAgent.acceleration = 50f;
+        visual = Instantiate(sheet.appearance.FBXPrefab, transform);
+        visual.transform.localPosition = sheet.appearance.position;
+        visual.transform.localEulerAngles = sheet.appearance.eulers;
+        visual.transform.localScale = sheet.appearance.scale;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /// debug
+        if (Input.GetMouseButtonDown(0)) {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                navMeshAgent.SetDestination(hit.point);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/SO/InhabitantSheet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(menuName = "Inhabitants/Sheet")]
+public class InhabitantSheet : ScriptableObject
+{
+    [System.Serializable]
+    public class Appearance
+    {
+        public GameObject FBXPrefab;
+        public Vector3 position;
+        public Vector3 eulers;
+        public Vector3 scale;
+    }
+
+    public Inhabitant.TEAM team;
+    public float walkingSpeed = 4f;
+    public float runningSpeed = 6f;
+    public Appearance appearance;
+    public int navMeshAreaMask;
+}
