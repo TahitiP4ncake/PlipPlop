08b718c261d05b05c3de2a5fb16d4525db0e4c13
Louis Perdu <l.perdu@rubika-edu.com>
2019-05-27 15:29:35 +0200

    Creating Scene switch system

---
+++ Assets/PresentationManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PresentationManager : MonoBehaviour
+{
+    public string[] scenes;
+    int current = 0;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        Load(current);
+    }
+
+    public void Next()
+    {
+        current++;
+        if(current >= scenes.Length) current = 0;
+        Load(current);
+    }
+
+    public void Previous()
+    {
+        current--;
+        if(current < 0) current = scenes.Length - 1;
+        Load(current);
+    }
+
+    void Load(int index)
+    {
+        SceneManager.LoadScene(scenes[index]);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow)) Next();
+        else if(Input.GetKeyDown(KeyCode.LeftArrow)) Previous();
+    }
+}
