6267ac3989eccf2608c82b72470f477e3a6f041f
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2019-05-27 18:28:36 +0200

    spriteCube

--- Assets/Scripts/Absorbable/SpriteCube.cs
+++ Assets/Scripts/Absorbable/SpriteCube.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class SpriteCube : Absorbable
 {
     public Transform toDisplay;
+    public AnimationCurve curve;
+    private bool visible;
 
     public override void Absorb(Absorber a)
     {
         base.Absorb(a);
-        
-    }   
+        StartCoroutine(ShowSprite());
+    }
 
-    public override void Release(Absorber a)
+    IEnumerator ShowSprite()
     {
-        base.Release(a);
-
+        visible = true;
+        float _y = 0;
+        while (_y < 1)
+        {
+            toDisplay.localScale = Vector3.Lerp(Vector3.zero, Vector3.one,curve.Evaluate(_y));
+            _y += Time.deltaTime*3;
+            yield return null;
+        }
+        
     }
 }
