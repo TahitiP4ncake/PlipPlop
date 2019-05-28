f3bb79936e2a2a3cdf27425c9c1bf01554502b41
Louis Perdu <l.perdu@rubika-edu.com>
2019-05-28 17:11:42 +0200

    That's the commit about water ...

---
+++ Assets/Water.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Water : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.gameObject.GetComponent<PlayerController>()) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
